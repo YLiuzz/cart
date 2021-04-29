1cf22d5b3861cd69e9b7added53077e950a713f2
Terry <tarry5508@gmail.com>
2021-04-29 20:39:36 +0800

    feat:add Cart validator

--- cart/app/Http/Controllers/CartItemController.php
+++ cart/app/Http/Controllers/CartItemController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class CartItemController extends Controller
 {
@@ -36,6 +37,20 @@ class CartItemController extends Controller
     public function store(Request $request)
     {
         $form = $request->all();
+        $message = [
+            'required' => ':attribute 是必要的',
+            'between'   =>':attribute 的輸入 :input 不在 :min 跟:max之間'
+        ];
+        $vilidator = Validator::make($form,[
+            'cart_id' => 'required|integer',
+            'product_id' => 'required|integer',
+            'quantity'  => 'required|integer|between:1,10'
+        ],$message);
+        if($vilidator->fails()){
+            return response($vilidator->errors(),400);
+        }
+        $vilidatorDate = $vilidator->validated();  //取得驗證通過Data
+
         DB::table('cart_items')->insert(['cart_id' => $form['cart_id'],
                                         'product_id'=>$form['product_id'],
                                         'quantity'  =>$form['quantity'],
